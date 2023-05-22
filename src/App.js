9f1cd61ee0d7c8a0fcc9ce6c95156a9654419718
cheng_oragne <2680618660@qq.com>
2023-05-22 14:06:01 +0800

    新增了根据精灵ID或名称搜索相关技能的功能

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SearchByName from './pages/SearchByName';
 import DIYSkill from './pages/DIYSkill';
 import { useState } from 'react';
 import SkillLib from './pages/SkillLib';
+import SearchByPet from "./pages/SearchByPet";
 function App () {
   const [active,setActive]=useState(1);
   return (
@@ -26,7 +27,7 @@ function App () {
             <li className={active==3? "active":'unactive'}><Link data-index={3} to="/searchByType">按属性查找</Link></li>
             <li className={active==4? "active":'unactive'}><Link data-index={4} to="/searchByName">按名称查找</Link></li>
             <li className={active==5? "active":'unactive'}><Link data-index={5} to="/DiySkill">自定义技能</Link></li>
-            <li className={active==6? "active":'unactive'}><Link data-index={6} to="/SkillLib">技能库</Link></li>
+            <li className={active==6? "active":'unactive'}><Link data-index={6} to="/SearchByPet">按精灵查找</Link></li>
             <li className={active==7? "active":'unactive'}><Link data-index={7} to="/SkillLib">技能库</Link></li>
             <li className={active==8? "active":'unactive'}><Link data-index={8} to="/SkillLib">技能库</Link></li>
             <li className={active==9? "active":'unactive'}><Link data-index={9} to="/SkillLib">技能库</Link></li>
@@ -42,6 +43,7 @@ function App () {
           <Route path="/searchByName" element={<SearchByName />} />;
           <Route path="/DiySkill" element={<DIYSkill />} />;
           <Route path="/SkillLib" element={<SkillLib />} />;
+          <Route path="/SearchByPet" element={<SearchByPet />} />;
         </Routes>
       </div>
 
---
+++ src/pages/SearchByPet.js
@@ -0,0 +1,61 @@
+import {Search, Toast} from "react-vant";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import SkillPanel from "../components/SkillPanel";
+
+function SearchByPet(){
+    const [value, setValue] = useState("");
+    const [page, setPage] = useState(0);
+    const [resultList, setResultList] = useState([]);
+    const [name,setName] = useState("");
+    useEffect(()=>{
+        if (value)
+         axios.get(`http://localhost:8080/getSkillByIdOrName?val=${value}`).then((res)=>{
+            setResultList(res.data);
+            console.log(res.data)
+             res.data.map(item=>{
+                 console.log(item.petsName.split("、")[item.petsID.split(',').indexOf(value)])
+                 setName(item.petsName.split("、")[item.petsID.split(',').indexOf(value)])
+             })
+        })
+        else return ()=>{}
+    },[value])
+
+    return (<div>
+        <Search
+            value={value}
+            style={{width:"100%"}}
+            placeholder="输入精灵的id或者名字，支持模糊搜索.按Enter以搜索"
+            showAction
+            onSearch={(val)=>{
+                Toast();
+                setValue(val);
+            }}
+        />
+        <div  >
+            <div>您正在查看<p style={{fontWeight:"bold",display:"inline"}}>{name? name:"有关"+"\""+value+"\""}</p>的技能</div>
+            {!resultList.length ? (
+                <p>Loading...</p>
+            ) : (
+                <div style={{display:"flex", flexWrap:"wrap"}}>
+                    {resultList.map((item) => (
+                    <SkillPanel
+                        key={item.id}
+                        shuxing={item.type}
+                        name={item.name}
+                        power={item.power}
+                        pp={item.maxPP}
+                        des={item.des}
+                        category={item.category}
+                        priority={item.priority}
+                        id={item.id}
+                        accuracy={item.accuracy}
+                        critRate={item.critRate}
+                        petsName={item.petsName}
+                    />))}
+            </div>
+            )}
+        </div>
+    </div>)
+}
+export default SearchByPet;
